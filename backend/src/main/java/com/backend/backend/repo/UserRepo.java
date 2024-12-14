e712dc351061a7ca710422b26aea482c4c38ec6e
MeetGJethva <23ceubd004@ddu.ac.in>
2024-12-14 21:22:41 +0530

    connected with mongodb atlas

--- backend/src/main/java/com/backend/backend/repo/UserRepo.java
+++ backend/src/main/java/com/backend/backend/repo/UserRepo.java
@@ -1,9 +1,11 @@
 package com.backend.backend.repo;
 
 import com.backend.backend.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepo extends JpaRepository<User, Long> {
+public interface UserRepo extends MongoRepository<User, Long> {
+
+
 }
