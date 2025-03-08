4b08b0e8b018794ad7fdca6d3c4eb10cfc7ed0f8
Parth <108926206+Parthkoringa@users.noreply.github.com>
2025-03-08 15:28:26 +0530

    Update Product.java

--- backend/src/main/java/com/backend/backend/models/Product.java
+++ backend/src/main/java/com/backend/backend/models/Product.java
@@ -17,7 +17,7 @@ public class Product {
     private String name;
     private String description;
     private Double price;
-    private byte[] imageUrl;
+    private String imageUrl;
     private int rating;
 
     public int getNo_of_ratings() {
